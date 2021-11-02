4c3ba88d6e42b7fd2bb2caaf8d6b29a8f4dea384
Sadia Uzair <sadia.uzair@devsinc.com>
2021-11-02 20:05:43 +0500

    tested app component

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,39 @@
-import { mount, shallow } from 'enzyme'
+import { mount } from 'enzyme'
 import { App } from 'App'
 
+import configureStore from 'redux-mock-store'
+import { Provider } from 'react-redux'
+
+let wrapper
+const setup = store =>
+  mount(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  )
+
+const mockStore = configureStore()
 describe('rendering components', () => {
-  it('app is rendering', () => {
-    const wrapper = shallow(<App />)
-    expect(wrapper.contains(<App />))
+  const initialState = {
+    ingredient: {
+      lettuce: 1,
+      bacon: 0,
+      cheese: 0,
+      meat: 0,
+      amount: 3.5
+    }
+  }
+
+  beforeEach(() => {
+    const store = mockStore(initialState)
+    wrapper = setup(store)
   })
 
-  // it('app is rendering', () => {
-  //   const wrapper = mount(<App />)
-  //   expect(wrapper.contains(<App />))
-  // })
+  it('App should render', () => {
+    expect(wrapper.length).toBe(1)
+  })
+
+  it('Burger should be rendered', () => {
+    expect(wrapper).toMatchSnapshot()
+  })
 })
--- src/containers/Burger/index.test.js
+++ src/containers/Burger/index.test.js
@@ -1,11 +1,11 @@
-import { shallow } from 'enzyme'
+import { mount } from 'enzyme'
 import { Burger } from 'containers'
 import configureStore from 'redux-mock-store'
 import { Provider } from 'react-redux'
 
 let wrapper
 const setup = store =>
-  shallow(
+  mount(
     <Provider store={store}>
       <Burger />
     </Provider>
@@ -14,11 +14,13 @@ const setup = store =>
 const mockStore = configureStore()
 describe('rendering components', () => {
   const initialState = {
-    lettuce: 1,
-    bacon: 0,
-    cheese: 0,
-    meat: 0,
-    amount: 3.5
+    ingredient: {
+      lettuce: 1,
+      bacon: 0,
+      cheese: 0,
+      meat: 0,
+      amount: 3.5
+    }
   }
 
   beforeEach(() => {
